4b83d2c8a15b9646660c4a008257f95cfa70eaeb
Michał Łuszczyk <michal.luszczyk@gmail.com>
2016-12-04 21:14:32 +0100

    Gauss elimination.

--- gauss.py
+++ gauss.py
@@ -2,6 +2,8 @@ import numpy
 
 
 def solvetopleft(mat, b):
+    assert mat.ndim == 2
+    assert b.ndim == 1
     assert mat.shape[0] == mat.shape[1]
     assert b.shape[0] == mat.shape[0]
     n = mat.shape[0]
@@ -14,3 +16,33 @@ def solvetopleft(mat, b):
         res[c] = (b[c] - numpy.inner(mat[c, c + 1:].reshape(-1), res[c + 1:].reshape(-1))) / mat[c, c]
     return res
 
+
+def triangulate(mat):
+    """Triangulizes a nonsingular matrix in place."""
+    assert mat.shape[0] <= mat.shape[1]
+    assert mat.ndim == 2
+
+    n = mat.shape[0]
+
+    for c in range(n):
+        assert (mat[c, :c] == 0).all(), "not elimanted correctly"
+        assert (mat[c, c] != 0).all(), "nonsingular matrix"
+        row = numpy.argmax(mat[c:, c]) + c
+        mat[[row, c]] = mat[[c, row]]
+        mat[c] /= mat[c, c]
+        for d in range(c + 1, n):
+            mat[d] -= mat[c] * mat[d, c]
+
+
+def solve(mat, b):
+    assert mat.ndim == 2
+    assert b.ndim == 1
+    assert mat.shape[0] == mat.shape[1]
+    assert b.shape[0] == mat.shape[0]
+    n = mat.shape[0]
+    b = b.reshape(-1, 1)
+    full = numpy.concatenate((mat, b), axis=1)
+    triangulate(full)
+    mat, b = full[:, :n], full[:, n:]
+    b = b.flatten()
+    return solvetopleft(mat, b)
--- tests.py
+++ tests.py
@@ -3,40 +3,73 @@ from unittest import TestCase
 import numpy
 from numpy.testing import assert_array_equal
 
-from gauss import solvetopleft
+from gauss import solvetopleft, triangulate, solve
 
 
 class TestSolveTopLeft(TestCase):
     def test_one_element(self):
-        result = solvetopleft(numpy.asmatrix([2.]), numpy.asmatrix([4.]))
-        assert_array_equal(result, numpy.asmatrix([2.]))
+        result = solvetopleft(numpy.asarray([[2.]]), numpy.asarray([4.]))
+        assert_array_equal(result, numpy.asarray([2.]))
 
     def test_id(self):
-        m = numpy.matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]], dtype='f')
-        b = numpy.matrix([3, 1, 5], dtype='f').T
+        m = numpy.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]], dtype='f')
+        b = numpy.array([3, 1, 5], dtype='f').T
         r = solvetopleft(m, b)
-        assert_array_equal(r, numpy.asmatrix([3, 1, 5]).T)
+        assert_array_equal(r, numpy.asarray([3, 1, 5]).T)
 
     def test_three_elements(self):
-        m = numpy.matrix([[1, 4, 2], [0, 1, 3], [0, 0, 1]], dtype='f')
+        m = numpy.array([[1, 4, 2], [0, 1, 3], [0, 0, 1]], dtype='f')
         b = numpy.array([-3, 11, 5], dtype='f')
         r = solvetopleft(m, b)
         assert_array_equal(r, numpy.array([3, -4, 5]))
 
     def test_division_on_diagonal(self):
-        m = numpy.matrix([[2, 0], [0, 1]], dtype='f')
+        m = numpy.array([[2, 0], [0, 1]], dtype='f')
         b = numpy.array([4, 1], dtype='f')
         r = solvetopleft(m, b)
         assert_array_equal(r, [2, 1])
 
     def test_not_triangular_last_row(self):
-        m = numpy.matrix([[2, 1], [1, 1]], dtype='f')
+        m = numpy.array([[2, 1], [1, 1]], dtype='f')
         b = numpy.array([1, 1], dtype='f')
         with self.assertRaises(Exception):
             solvetopleft(m, b)
 
     def test_not_triangular(self):
-        m = numpy.matrix([[2, 1, 0], [1, 1, 0], [0, 0, 1]], dtype='f')
+        m = numpy.array([[2, 1, 0], [1, 1, 0], [0, 0, 1]], dtype='f')
         b = numpy.array([1, 1, 1], dtype='f')
         with self.assertRaises(Exception):
             solvetopleft(m, b)
+
+
+class TestTriangulate(TestCase):
+    def test_one_element(self):
+        r = numpy.array([[2., 2.]], dtype='f')
+        triangulate(r)
+        assert_array_equal(r, numpy.array([[1., 1.]], dtype='f'))
+
+    def test_two_elements(self):
+        m = numpy.array([[2., 2., 4.], [1., 2., 0.]], dtype='f')
+        triangulate(m)
+        expected = numpy.array([[1., 1., 2.], [0., 1., -2]], dtype='f')
+        assert_array_equal(m, expected)
+
+    def test_nonsingular(self):
+        m = numpy.array([[1., 2., 1.], [2., 4., 2.]])
+        with self.assertRaises(Exception):
+            triangulate(m)
+
+    def test_element_choice(self):
+        m = numpy.array([[2., 4.], [3., 9.]])
+        triangulate(m)
+        expected = numpy.array([[1., 3.], [0., 1.]])
+        assert_array_equal(m, expected)
+
+
+class TestGauss(TestCase):
+    def test(self):
+        m = numpy.array([[1., 2., 3.], [0., 1., 2.], [0., 0., 1.]])
+        b = numpy.array([10., 4., 1.])
+        result = solve(m, b)
+        expected = numpy.array([3., 2., 1.])
+        assert_array_equal(result, expected)
